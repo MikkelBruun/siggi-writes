e62d6b5426d4d5c50ef50d0823301f4798023979
MikkelBruun <mikkelbruunibsen@gmail.com>
2024-07-22 16:48:27 +0200

    Add sloppy build
    for testing purposes

---
+++ _next/static/chunks/app/layout-b99d793a0b77a5ea.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5098:function(n,e,u){Promise.resolve().then(u.t.bind(u,6782,23))},6782:function(){}},function(n){n.O(0,[662,971,23,744],function(){return n(n.s=5098)}),_N_E=n.O()}]);
